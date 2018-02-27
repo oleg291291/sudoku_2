b11e3c9621438457b67ffe9fa2a5f907d1fa00a2
oleg291291@mail.ru <oleg291291@mail.ru>
2018-02-27 22:09:12 +0300

    step1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
 module.exports = function solveSudoku(matrix) {
   // your solution
+  var candArr = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ];
+  function singleFinder() {
+    for (var row = 0; row < 9; row++) {
+      for (var col = 0; col < 9; col++) {
+        if (matrix[row][col] != 0) {
+          candArr[row][col] = matrix[row][col];
+        }
+        if (matrix[row][col] == 0) {
+          var inField = [];
+          for (var i = 0; i < 9; i++) {
+            inField.push(matrix[row][i]);
+            inField.push(matrix[i][col]);
+          }
+          //добавить квадрат
+          var sectRow = Math.floor(row / 3) * 3;
+          var sectCol = Math.floor(col / 3) * 3;
+
+          for (var j = 0; j < 3; j++) {
+            for (var h = 0; h < 3; h++) {
+              inField.push(matrix[sectRow + j][sectCol + h]);
+            }
+          }
+          inField = inField.join("");
+          console.log(inField)
+          
+          var cellCand = "";
+          
+          for (var x = 1; x < 10; x++){
+            if(inField.indexOf(x) == -1){
+              cellCand += x;
+            }
+          }
+          if(cellCand.length == 1){
+            matrix[row][col] = +cellCand[0]
+            console.log(matrix[row]);
+          }
+         
+          
+          
+
+        }
+      }
+    }
+
+  }
+
+singleFinder();
+
+
 }
+
